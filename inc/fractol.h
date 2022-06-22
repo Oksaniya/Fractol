3d62a7b6b3b8a08b26fa026a07114d47cbca9d00
Oksaniya <78662225+Oksaniya@users.noreply.github.com>
2022-06-22 09:59:06 +0200

    The first pixels

--- inc/fractol.h
+++ inc/fractol.h
@@ -1,7 +1,7 @@
 #ifndef FRACTOL_H
 #define FRACTOL_H
 
-#include "../SDL/include/SDL.h"
+#include <SDL2/SDL.h>
 #include "../my_stdlib/header/my_stdlib.h"
 
 
--- src/main.c
+++ src/main.c
@@ -1,8 +1,22 @@
 #include <SDL2/SDL.h>
 
 #define TRUE 1
-#define WINDOE_WiDTH 750
+#define WINDOW_WiDTH 750
 #define WINDOW_HEIGHT 500
+
+#define RED 2
+#define GREEN 1
+#define BLUE 0
+#define OPACITY 3
+
+typedef union   s_pixel
+{
+    uint8_t     channel[4];
+    uint32_t    color;
+}               t_pixel;
+
+void set_pixel(SDL_Surface *surface, int x, int y, t_pixel pixel);
+
 
 int main(void)
 {
@@ -10,11 +24,10 @@ int main(void)
     SDL_Event       window_event;
     SDL_Surface		*surface;
     char            window_name[] = "my_window";
-    uint8_t         *pixels;
+    t_pixel         pixel;
 
     my_window = NULL;
     surface = NULL;
-    pixels = NULL;
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
@@ -23,7 +36,7 @@ int main(void)
     my_window = SDL_CreateWindow(window_name, 
                                 SDL_WINDOWPOS_CENTERED, 
                                 SDL_WINDOWPOS_CENTERED, 
-                                WINDOE_WiDTH, 
+                                WINDOW_WiDTH, 
                                 WINDOW_HEIGHT, 
                                 SDL_WINDOW_ALLOW_HIGHDPI);
 
@@ -33,30 +46,29 @@ int main(void)
         return EXIT_FAILURE;
     }
     surface = SDL_GetWindowSurface(my_window);
+
+    /* init surface with 0 */
     SDL_LockSurface(surface);
     SDL_memset(surface->pixels, 0, surface->h * surface->pitch);
     SDL_UnlockSurface(surface);
     SDL_UpdateWindowSurface(my_window);
-    // SDL_FillRect(/*3 arguments*/); // poczytaj na wiki, pyszut szczo szwydsze praciuje niż wrucznu
-    //SDL_LockSurface(surface);
-    pixels = (uint8_t *)surface->pixels;
-    //SDL_UnlockSurface(surface);
+
+    /* set pixel in the center of the window to blue */
+    pixel.channel[RED] = 0xff;
+    pixel.channel[GREEN] = 0xff;
+    pixel.channel[BLUE] = 0xff;
+    pixel.channel[OPACITY] = 0xff;
     SDL_LockSurface(surface);
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     for (int j = 0; j < 5; j++)
-    //     {
-    //         pixels[i * surface->pitch + j + 10] = 255;
-    //     }
-    // }
-    //memcpy(surface->pixels, pixels, surface->pitch * surface->h);
-    for (int i = 250; i < 500; i++)
+    for (int x = 300; x < 450; x++)
     {
-        pixels[i + 750] = 255;
+        for (int y = 175; y < 325; y++)
+        {
+            set_pixel(surface, x, y, pixel);
+        }
     }
     SDL_UnlockSurface(surface);
     SDL_UpdateWindowSurface(my_window);
-    
+
     while (TRUE)
     {
         if (SDL_PollEvent(&window_event))
@@ -74,3 +86,11 @@ int main(void)
 }
 //clang -c src/main.c -o obj/main.o -F/Library/Frameworks
 //clang obj/main.o -o fractol -F/Library/Frameworks -framework SDL2
+
+void set_pixel(SDL_Surface *surface, int x, int y, t_pixel pixel)
+{
+  Uint32 * const target_pixel = (Uint32 *) ((Uint8 *) surface->pixels
+                                             + y * surface->pitch
+                                             + x * surface->format->BytesPerPixel);
+  *target_pixel = pixel.color;
+}
